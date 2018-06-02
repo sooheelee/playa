a80ea7f3dbd48f3773b809641440f0f97eee23e7
Soo Hee Lee <shlee0909@gmail.com>
2018-06-01 20:51:44 -0400

    Finalize bluetooth

--- app/src/main/java/com/example/android/playa/MainActivity.java
+++ app/src/main/java/com/example/android/playa/MainActivity.java
@@ -2,39 +2,22 @@ package com.example.android.playa;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
-import android.app.Activity;
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.ListView;
-import android.widget.MediaController;
 import android.widget.TextView;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
 import android.widget.Toast;
-import android.widget.VideoView;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
-
-    private final static int REQUEST_ENABLE_BT = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,56 +52,40 @@ public class MainActivity extends AppCompatActivity {
 //        });
 
         ListView bluetooth = findViewById(R.id.bluetooth);
+        TextView deviceList = findViewById(R.id.device_list);
 
-        BroadcastReceiver mReceiver = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                String action = intent.getAction();
-                if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-                    BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                    Log.v("BOOTOOTH", "Device found: " + device.getName() + device.getAddress());
-                }
-            }
-        };
 
         BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        if (mBluetoothAdapter != null && mBluetoothAdapter.isEnabled()) {
-            Toast.makeText(this, "Bluetooth is on", Toast.LENGTH_SHORT).show();
-            Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
-            //        if(pairedDevices.size()>0)
-//
-//        {
-            // from https://stackoverflow.com/questions/10795424/how-to-get-the-bluetooth-devices-as-a-list
-            List<String> listOfBluetoothDevices = new ArrayList<String>();
-            for (BluetoothDevice isbluetoothDevice : pairedDevices) {
-                String deviceName = isbluetoothDevice.getName().toString();
-                String deviceHardwareAddress = isbluetoothDevice.getAddress();
-                String deviceNameAndAddress = deviceName + deviceHardwareAddress;
-                listOfBluetoothDevices.add(deviceNameAndAddress);
-            }
-            ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
-                    this, android.R.layout.simple_list_item_1,
-                    listOfBluetoothDevices
-            );
-            bluetooth.setAdapter(arrayAdapter);
-//        }
-//
-//            listView.setOnItemSelectedListener (new AdapterView.OnItemSelectedListener() {
-//
-//                @Override
-//                public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
-//
-//                }
-//            });
 
-        } else{
-            Toast.makeText(this, "Bluetooth is off", Toast.LENGTH_SHORT).show();
+        if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            int REQUEST_ENABLE_BT = 1;
             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            deviceList.setText("For bluetooth functions, turn bluetooth on and restart app.");
         }
 
-        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-        registerReceiver(mReceiver, filter);
+        // Lists only known, i.e. previously paired bluetooth devices, that are available.
+        // Which is connected is a fuction yet to be wired.
+        if (mBluetoothAdapter != null && mBluetoothAdapter.isEnabled()) {
+            Toast.makeText(this, "Bluetooth is on", Toast.LENGTH_SHORT).show();
+        }
+
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+        List<String> listOfBluetoothDevices = new ArrayList<String>();
+        for (BluetoothDevice isbluetoothDevice : pairedDevices) {
+            String deviceName = isbluetoothDevice.getName();
+            String deviceHardwareAddress = isbluetoothDevice.getAddress();
+            int isBondBonded = isbluetoothDevice.getType();
+            String deviceNameAndAddress = deviceName + "\n" + deviceHardwareAddress + "\n" + String.valueOf(isBondBonded);
+            listOfBluetoothDevices.add(deviceNameAndAddress);
+        }
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_list_item_1,
+                listOfBluetoothDevices
+        );
+        bluetooth.setAdapter(arrayAdapter);
+    }
+}
 
 
 //        // from https://stackoverflow.com/questions/4458529/mediacontroller-always-show-on-android
@@ -128,31 +95,8 @@ public class MainActivity extends AppCompatActivity {
 //        videoView.setMediaController(mediaController);
 //
 //        {
-//            // from https://stackoverflow.com/questions/10795424/how-to-get-the-bluetooth-devices-as-a-list
-//            List<String> listOfBluetoothDevices = new ArrayList<String>();
-//            for (BluetoothDevice isblueToothDevice : pairedDevices) {
-//                String deviceName = isblueToothDevice.getName();
-//                String deviceHardwareAddress = isblueToothDevice.getAddress();
-//                listOfBluetoothDevices.add(deviceName, deviceHardwareAddress);
-//            }
 //
-//            ListViewAdapter bluetoothListViewAdapter = new ListViewAdapter(this, listOfBluetoothDevices);
-//            ListView listView = findViewById(R.id.bluetooth);
-//            listView.setAdapter(bluetoothListViewAdapter);
-//
-//            listView.setOnItemSelectedListener (new AdapterView.OnItemSelectedListener() {
-//
-//                @Override
-//                public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
-//
-//                }
-//            });
-//            arg0;
-//            arg1;
-//            arg2;
-//            arg3;
-//            Intent enableBluetoothIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-        }
+//            listView.setOnItemSelectedListener
 
 //
 //        MediaController player = findViewById(R.id.player);
@@ -163,4 +107,4 @@ public class MainActivity extends AppCompatActivity {
 //                startActivity(playerIntent);
 //            }
 //        });
-    }
+
